07a0633cbf10f991809124ddbe51222955bd1467
Nick Melekian <nick.melekian@gmail.com>
2023-09-28 09:23:07 -0400

    Initial Commit

---
+++ a;lsdkfasd;lfka/a_lsdkfasd_lfkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  a_lsdkfasd_lfkaApp.swift
+//  a;lsdkfasd;lfka
+//
+//  Created by Nick Melekian on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct a_lsdkfasd_lfkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
